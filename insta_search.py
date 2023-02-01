dd1f90ff2d83977b61b17f00d109b4b002cfcc7e
ecemdenizb <ecemdenizbabaoglan@gmail.com>
2023-02-01 18:57:31 +0300

    tire belediyesi gecen insta postlarini google searchten buluyor. Zamana göre sıralama, linkleri çekme ve db olusturma eklenicek

--- insta_search.py
+++ insta_search.py
@@ -16,15 +16,19 @@ s = Service(PATH)
 
 
 def main():
-    keyword = "tire"
+    keyword1 = "tire"
+    keyword2 = "belediye"
     # URL = f"https://google.com/search?q={keyword}"
-    URL = f"https://www.google.com/search?q=site%3Ahttps%3A%2%2Fwww.instagram.com%2Fp%2F+**{keyword}**"
-    
+   # URL = f"https://www.google.com/search?q=site:www.instagram.com+{keyword1}"
+    #URL_dummy=f"https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjruJm13PP8AhWKQPEDHXHBBsMQFnoECA8QAQ&url=https%3A%2F%2Fwww.instagram.com%2Fp%2F{keyword1}%2F"
+   # URL=f'https://www.google.com/search?q=site=instagram.com+url=https://instagram.com/p/+"{keyword1}"'
+    URL = f'https://www.google.com/search?q=site:https://instagram.com/p/+"{keyword1}+AROUND+{keyword2}"'
     driver = webdriver.Chrome(executable_path=PATH)
-    
     driver.get(URL)
+    
     time.sleep(30)
-
+    driver.quit()
+    #linkler = driver.find_elements(by=By.TAG_NAME, value="h3")
 
 if __name__ == "__main__":
     main()
